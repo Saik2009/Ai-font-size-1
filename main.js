a0e67ddfa6217243b145c4a3a74b92f7cc6ea25f
Saik2009 <80210806+Saik2009@users.noreply.github.com>
2022-05-08 09:59:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+left_wrist=0;
+right_wrist=0;
+difference=0;
 function setup()
 {
     video= createCapture(VIDEO);
@@ -9,6 +12,8 @@ function setup()
 }
 function draw(){
     background('whitesmoke');
+    text('Saikrish',10,100,difference,difference);
+    
 }
 function modelLoaded(){
     console.log("Model is loaded!");
@@ -17,5 +22,11 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+
+         //wrist
+         left_wrist=results[0].pose.leftWrist.x;
+         right_wrist=results[0].pose.rightWrist.x;
+         console.log("left_wrist= "+left_wrist+" right_wrist= "+right_wrist);
+         difference=floor(left_wrist-right_wrist);
     }
 }
